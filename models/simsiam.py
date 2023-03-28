48a8626ec41171f7cc92475dd082e69d0541eb7b
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2023-03-28 09:06:44 -0400

    update doc stings and comments

--- models/simsiam.py
+++ models/simsiam.py
@@ -4,7 +4,9 @@ import torch.nn.functional as F
 from torchvision.models import resnet50
 
 
-def D(p, z, version='simplified'): # negative cosine similarity
+def D(p, z, version='simplified'):
+    """ The negative cosine similarity.
+    """
     if version == 'original':
         z = z.detach() # stop gradient
         p = F.normalize(p, dim=1) # l2-normalize 
@@ -17,13 +19,11 @@ def D(p, z, version='simplified'): # negative cosine similarity
 
 
 class projection_MLP(nn.Module):
-
+    ''' Paper page 3 baseline setting: Projection MLP.
+    The projection MLP (in f) has BN applied to each fully-connected (fc) layer, including its
+    output fc. Its output fc has no ReLU. The hidden fc is 2048-d. This MLP has 3 layers.
+    '''
     def __init__(self, in_dim, hidden_dim=2048, out_dim=2048):
-        ''' Paper page 3 baseline setting: Projection MLP.
-        The projection MLP (in f) has BN applied to each fully-connected (fc) layer,
-        including its output fc. Its output fc has no ReLU. The hidden fc is 2048-d.
-        This MLP has 3 layers.
-        '''
         super().__init__()
         self.layer1 = nn.Sequential(
             nn.Linear(in_dim, hidden_dim),
@@ -48,34 +48,34 @@ class projection_MLP(nn.Module):
 
 
 class prediction_MLP(nn.Module):
+    ''' Paper page 3 baseline setting: Prediction MLP.
+    The prediction MLP (h) has BN applied to its hidden fc layers. Its output fc does not have BN
+    (ablation in Sec. 4.4) or ReLU. This MLP has 2 layers. The dimension of h's input and output
+    (z and p) is d = 2048, and h's hidden layer's dimension is 512, making h a bottleneck structure
+    (ablation in supplement).
+    '''
     def __init__(self, in_dim=2048, hidden_dim=512, out_dim=2048): # bottleneck structure
         super().__init__()
-        ''' page 3 baseline setting
-        Prediction MLP. The prediction MLP (h) has BN applied 
-        to its hidden fc layers. Its output fc does not have BN
-        (ablation in Sec. 4.4) or ReLU. This MLP has 2 layers. 
-        The dimension of h’s input and output (z and p) is d = 2048, 
-        and h’s hidden layer’s dimension is 512, making h a 
-        bottleneck structure (ablation in supplement). 
-        '''
+
         self.layer1 = nn.Sequential(
             nn.Linear(in_dim, hidden_dim),
             nn.BatchNorm1d(hidden_dim),
             nn.ReLU(inplace=True)
         )
         self.layer2 = nn.Linear(hidden_dim, out_dim)
-        """
-        Adding BN to the output of the prediction MLP h does not work
-        well (Table 3d). We find that this is not about collapsing. 
-        The training is unstable and the loss oscillates.
-        """
+        # Adding BN to the output of the prediction MLP h does not work
+        # well (Table 3d). We find that this is not about collapsing. 
+        # The training is unstable and the loss oscillates.
 
     def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
         return x 
 
+
 class SimSiam(nn.Module):
+    """ The SimSiam model: encoder f (backbone + projector) + predictor h.
+    """
     def __init__(self, backbone=resnet50()):
         super().__init__()
 
@@ -93,10 +93,6 @@ class SimSiam(nn.Module):
         p2 = self.predictor(z2)  # p2 = h(z2)
         L = D(p1, z2) / 2 + D(p2, z1) / 2
         return {'loss': L}
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -118,21 +114,3 @@ if __name__ == "__main__":
     print(D(z1, z2, version='simplified'))
     toc = time.time()
     print(toc - tic)
-
-# Output:
-# tensor(-0.0010)
-# 0.005159854888916016
-# tensor(-0.0010)
-# 0.0014872550964355469
-
-
-
-
-
-
-
-
-
-
-
-
