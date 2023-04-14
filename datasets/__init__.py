e49e508feabb536d34cbe38a3fb1855496ada5ee
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2023-04-13 20:31:15 -0400

    get_stl10

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,9 +1,18 @@
 from .paired_lensing_dataset import PairedLensingImageDataset
+from .stl10 import get_stl10
 
 
 def get_dataset(dataset_name, data_dir):
     if dataset_name == 'paired-lensing':
         dataset = PairedLensingImageDataset(data_dir)
+    else:
+        raise NotImplementedError
+    return dataset
+
+
+def get_umap_testset(dataset_name, **kwarg):
+    if dataset_name == 'STL10':
+        dataset = get_stl10(**kwarg)
     else:
         raise NotImplementedError
     return dataset
