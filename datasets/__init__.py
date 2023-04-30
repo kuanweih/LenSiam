09ec807819e8e4879d4bec7dda03ed26b877edff
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2023-04-30 08:49:46 -0400

    add RealHST and subset size to train set

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,12 +1,16 @@
+import torch
+
 from .paired_lensing_dataset import PairedLensingImageDataset
-from .umap_testsets import get_stl10, get_2022_lens_geoff
+from .umap_testsets import get_stl10, get_2022_lens_geoff, get_real_hst
 
 
-def get_dataset(dataset_name, data_dir):
+def get_dataset(dataset_name, data_dir, subset_size=None):
     if dataset_name == 'paired-lensing':
         dataset = PairedLensingImageDataset(data_dir)
     else:
         raise NotImplementedError
+    if subset_size is not None:
+        dataset = torch.utils.data.Subset(dataset, list(range(subset_size)))
     return dataset
 
 
@@ -15,6 +19,8 @@ def get_umap_testset(dataset_name, **kwarg):
         dataset = get_stl10(**kwarg)
     elif dataset_name == 'Lens2022':
         dataset = get_2022_lens_geoff(**kwarg)
+    elif dataset_name == 'RealHST':
+        dataset = get_real_hst(**kwarg)
     else:
         raise NotImplementedError
     return dataset
