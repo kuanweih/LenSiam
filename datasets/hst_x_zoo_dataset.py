1d3b38b1060df279b31709bbcaad7a31ce035334
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2023-05-06 19:35:13 -0400

    init

---
+++ datasets/hst_x_zoo_dataset.py
@@ -0,0 +1,65 @@
+import glob
+import os
+import torch
+import numpy as np
+
+from PIL import Image
+from torchvision import transforms
+from torch.utils.data import Dataset
+from .parameters import image_size, imagenet_mean_std
+
+
+def get_hst_x_zoo(root, subset_size=None):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((image_size, image_size)),
+        transforms.Lambda(lambda x:(x - x.min()) / (x.max() - x.min())),  # normalized [0, 1]
+        transforms.Normalize(*imagenet_mean_std),
+    ])
+    dataset = HSTxZOODataset(root=root, transform=transform)
+    if subset_size is not None:
+        dataset = torch.utils.data.Subset(dataset, list(range(subset_size)))
+    return dataset
+
+
+
+class HSTxZOODataset(Dataset):
+    """ The joint set of HST dataset and Galaxy Zoo dataset created by
+        https://github.com/kuanweih/lensed_quasar_database_scraper
+
+    Args:
+        Dataset: torch.utils.data.Dataset class
+    """
+    def __init__(self, root, transform=None):
+        """ Initialize the class.
+
+        Args:
+            root (str): dir of the dataset
+            transform (torchvision.transforms, optional): transforms for images. Defaults to None.
+        """
+        self.root = root
+        self.transform = transform
+        self.class_map = {"zoo" : 0, "hst": 1}
+        self.data = []
+        for class_path in glob.glob(os.path.join(self.root, "*")):
+            if not os.path.isdir(class_path):
+                continue
+            class_name = os.path.basename(class_path)
+            for img_path in glob.glob(os.path.join(class_path, "*.jpg")):
+                self.data.append([img_path, class_name])
+        self.size = len(self.data)
+
+    def __getitem__(self, index):
+        if index >= self.size:
+            raise Exception
+        img_path, class_name = self.data[index]
+        image = Image.open(img_path)
+        image = image.resize((image_size, image_size))
+        image = np.array(image).astype("float32")
+        image = self.transform(image)
+        class_id = self.class_map[class_name]
+        class_id = torch.Tensor([class_id])
+        return image, class_id
+
+    def __len__(self):
+        return self.size
